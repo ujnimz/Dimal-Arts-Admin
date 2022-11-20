75efe4845176cdb61271f7b04accba2be198b575
Ujith Nimantha <ujithnimantha@gmail.com>
2022-11-20 17:48:03 +0300

    Updated database client

--- config/database.js
+++ config/database.js
@@ -21,7 +21,7 @@ module.exports = ({ env }) => ({
     client: env("DATABASE_TYPE", "mysql"),
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 3306),
+      port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "strapi"),
       password: env("DATABASE_PASSWORD", "strapi"),
